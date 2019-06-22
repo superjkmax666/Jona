1d967e4307976c6cb3ba36d710b053ee74ae7b01
John Al-Khoury <superjkmax666@gmail.com>
2019-06-22 22:19:41 +0300

    Added Input Polling

--- Jona/src/Jona/Application.cpp
+++ Jona/src/Jona/Application.cpp
@@ -6,6 +6,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Jona {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -59,6 +61,9 @@ namespace Jona {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto[x, y] = Input::GetMousePosition();
+			JN_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
---
+++ Jona/src/Jona/Input.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Jona/Core.h"
+
+namespace Jona {
+
+	class JONA_API Input 
+	{
+	public:
+		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
+
+
+		inline static bool IsMouseButtonPressedbutton(int button) { return s_Instance->IsMouseButtonPressedbuttonImpl(button); }
+		inline static std::pair<float,float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
+		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
+		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) = 0;
+
+		virtual bool IsMouseButtonPressedbuttonImpl(int button) = 0;
+		virtual std::pair<float, float> GetMousePositionImpl() = 0;
+		virtual float GetMouseXImpl() = 0;
+		virtual float GetMouseYImpl() = 0;
+	private:
+		static Input* s_Instance;
+	};
+
+}
