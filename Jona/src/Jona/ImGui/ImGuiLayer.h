ef262eba419d70a92c83032c0ff4e8144607503f
John Al-Khoury <superjkmax666@gmail.com>
2019-06-22 10:43:07 +0300

    Added Events to ImGui

--- Jona/src/Jona/ImGui/ImGuiLayer.h
+++ Jona/src/Jona/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Jona/Layer.h"
 
+#include "Jona/Events/ApplicationEvent.h"
+#include "Jona/Events/KeyEvent.h"
+#include "Jona/Events/MouseEvent.h"
+
 namespace Jona {
 
 	class JONA_API ImGuiLayer : public Layer
@@ -14,6 +18,16 @@ namespace Jona {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
