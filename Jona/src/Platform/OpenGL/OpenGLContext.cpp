8b427a4004237c655b3c452be2a0802d76ca7a72
John Al-Khoury <superjkmax666@gmail.com>
2019-07-08 00:19:58 +0300

    Drew first triangle and Added GPU info output

--- Jona/src/Platform/OpenGL/OpenGLContext.cpp
+++ Jona/src/Platform/OpenGL/OpenGLContext.cpp
@@ -16,6 +16,11 @@ namespace Jona {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 		JN_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		JN_CORE_INFO("OpenGL Info:");
+		JN_CORE_INFO("	Vendor: {0}", glGetString(GL_VENDOR));
+		JN_CORE_INFO("	Renderer: {0}", glGetString(GL_RENDERER));
+		JN_CORE_INFO("	Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers() {
