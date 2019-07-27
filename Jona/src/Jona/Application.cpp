a5d170663dee60aba6b1c3363da46a617eb8cd7c
John Al-Khoury <superjkmax666@gmail.com>
2019-07-27 22:23:55 +0300

    Added buffer layout descriptors for vertex buffers

--- Jona/src/Jona/Application.cpp
+++ Jona/src/Jona/Application.cpp
@@ -13,6 +13,25 @@ namespace Jona {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
+	
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+		switch (type) {
+			case Jona::ShaderDataType::Float:		return GL_FLOAT;
+			case Jona::ShaderDataType::Float2:		return GL_FLOAT;
+			case Jona::ShaderDataType::Float3:		return GL_FLOAT;
+			case Jona::ShaderDataType::Float4:		return GL_FLOAT;
+			case Jona::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Jona::ShaderDataType::Mat4:		return GL_FLOAT;
+			case Jona::ShaderDataType::Int:			return GL_INT;
+			case Jona::ShaderDataType::Int2:		return GL_INT;
+			case Jona::ShaderDataType::Int3:		return GL_INT;
+			case Jona::ShaderDataType::Int4:		return GL_INT;
+			case Jona::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		JN_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 
 	Application::Application()
 	{
@@ -31,16 +50,35 @@ namespace Jona {
 
 		// Vertex Buffer
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		// Index Buffer
 
@@ -52,12 +90,15 @@ namespace Jona {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -68,10 +109,12 @@ namespace Jona {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
--- Jona/src/Platform/OpenGL/OpenGLBuffer.h
+++ Jona/src/Platform/OpenGL/OpenGLBuffer.h
@@ -9,10 +9,14 @@ namespace Jona {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer: public IndexBuffer {
@@ -20,8 +24,8 @@ namespace Jona {
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 	private:
